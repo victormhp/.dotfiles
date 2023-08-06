911b4d661cf6b8b12239cb0007be43b674ba775e
victormhp <victoralbertomhp@gmail.com>
2023-08-05 21:19:58 -0600

    Add another theme to terminal

--- install.sh
+++ install.sh
@@ -15,6 +15,6 @@ link_same_files  "$(pwd)/nvim"            "$HOME/.config/nvim"
 link_same_single "$(pwd)" '.tmux.conf'    "$HOME"
 link_same_single "$(pwd)/zsh" '.zshrc'    "$HOME"
 link_same_single "$(pwd)/zsh" '.zsh_plugins.txt'    "$HOME"
-link_same_single "$(pwd)" '$HOME/.config/starship.toml' "$HOME"
+link_same_single "$(pwd)" 'starship.toml' "$HOME/.config"
 
 echo -e "\033[0;32mLinked all configuration files\033[0m"
