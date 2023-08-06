68245a903aa11e634a3b04cd8e861659da77807b
victormhp <victoralbertomhp@gmail.com>
2023-08-05 21:21:25 -0600

    Update links

--- install.sh
+++ install.sh
@@ -6,10 +6,13 @@ source './utils.sh'
 
 # Link entire configuration folders
 link_same        "$(pwd)/alacritty"       "$HOME/.config/alacritty"
+link_same        "$(pwd)/polybar"         "$HOME/.config/polybar"
+link_same        "$(pwd)/picom"           "$HOME/.config/picom"
 
 # Link just files instead of entire folders to avoid
 # polluting this dotfiles directory with generated files
 link_same_files  "$(pwd)/nvim"            "$HOME/.config/nvim"
+link_same_files  "$(pwd)/i3"              "$HOME/.config/i3"
 
 # Link just one file
 link_same_single "$(pwd)" '.tmux.conf'    "$HOME"
