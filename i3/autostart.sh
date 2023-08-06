9f006003e779e827a58241f555751d52c724ebdb
victormhp <victoralbertomhp@gmail.com>
2023-08-05 21:20:24 -0600

    Add i3 config

---
+++ i3/autostart.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Get the names of connected monitors
+connected_monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
+
+# Iterate through the connected monitors
+for monitor in $connected_monitors; do
+    # Skip the laptop screen
+    if [ "$monitor" != "eDP" ]; then
+        xrandr --output HDMI-A-0 --primary
+        xrandr --output "$monitor" --left-of eDP
+        break
+    fi
+done
+
+nitrogen --restore &
+picom
+
+$HOME/.config/polybar/launch.sh &
+
