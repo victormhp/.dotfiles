1517bb763d2c4e680499bf13bdabf3944d12b80d
victormhp <victoralbertomhp@gmail.com>
2024-01-27 00:48:06 -0600

    Polybar on all monitors

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-# Get the names of connected monitors
-connected_monitors=$(xrandr --query | grep " connected" | cut -d" " -f1)
-
-# Iterate through the connected monitors
-for monitor in $connected_monitors; do
-    # Skip the laptop screen
-    if [ "$monitor" != "eDP" ]; then
-        xrandr --output HDMI-A-0 --primary
-        xrandr --output "$monitor" --left-of eDP
-        break
-    fi
-done
-
 nitrogen --restore &
 
 killall polybar
-polybar -c /home/pollo/.config/polybar/config.ini &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload  &
+
+    if [ $"m" != "eDP" ]; then
+        xrandr --output "$m" --primary
+        xrandr --output "$m" --left-of eDP
+    fi
+done
 
 picom
