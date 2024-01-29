03d8c6222c369862a83320c699934a61aeed50ef
victormhp <victoralbertomhp@gmail.com>
2024-01-28 20:15:33 -0600

    Update config

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -11,10 +11,10 @@ return {
 
 			map("n", "<leader>a", function() harpoon:list():append() end)
 			map("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-			map("n", "<C-t>", function() harpoon:list():select(2) end)
 			map("n", "<C-h>", function() harpoon:list():select(1) end)
-			map("n", "<C-n>", function() harpoon:list():select(3) end)
-			map("n", "<C-s>", function() harpoon:list():select(4) end)
+			map("n", "<C-j>", function() harpoon:list():select(2) end)
+			map("n", "<C-k>", function() harpoon:list():select(3) end)
+			map("n", "<C-l>", function() harpoon:list():select(4) end)
 		end,
 	},
 }
