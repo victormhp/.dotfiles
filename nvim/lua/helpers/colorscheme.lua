04f421f63743764c6b014c160b365cd780d3c1bf
victormhp <victoralbertomhp@gmail.com>
2025-01-15 23:37:52 -0600

    Change theme

--- nvim/lua/helpers/colorscheme.lua
+++ nvim/lua/helpers/colorscheme.lua
@@ -16,10 +16,7 @@ local function get_if_available(name, opts)
 end
 
 -- Uncomment the colorscheme to use
--- local colorscheme = get_if_available("catppuccin")
--- local colorscheme = get_if_available('gruvbox')
-local colorscheme = get_if_available('rose-pine')
--- local colorscheme = get_if_available('everforest')
--- local colorscheme = get_if_available('melange')
+local colorscheme = get_if_available('carbonfox')
+-- local colorscheme = get_if_available('rose-pine')
 
 return colorscheme
