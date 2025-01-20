4a0803e9ff75de52fa19d4df9b97b2c789cb96c8
victormhp <victoralbertomhp@gmail.com>
2025-01-19 22:18:22 -0600

    Update harpoon

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -8,24 +8,6 @@ return {
 			harpoon:setup()
 
 			local map = require("helpers.keys").map
-			-- local conf = require("telescope.config").values
-			-- local function toggle_telescope(harpoon_files)
-			-- 	local file_paths = {}
-			-- 	for _, item in ipairs(harpoon_files.items) do
-			-- 		table.insert(file_paths, item.value)
-			-- 	end
-			--
-			-- 	require("telescope.pickers")
-			-- 		.new({}, {
-			-- 			prompt_title = "Harpoon",
-			-- 			finder = require("telescope.finders").new_table({
-			-- 				results = file_paths,
-			-- 			}),
-			-- 			previewer = conf.file_previewer({}),
-			-- 			sorter = conf.generic_sorter({}),
-			-- 		})
-			-- 		:find()
-			-- end
 
 			map("n", "<leader>a", function()
 				harpoon:list():add()
