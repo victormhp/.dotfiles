e447765bff97ebc8a29ce5b4098e522a96c5c4f8
victormhp <victoralbertomhp@gmail.com>
2025-01-20 14:22:19 -0600

    Update rofi styles

--- rofi/launcher.sh
+++ rofi/launcher.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/rofi/rounded-gray-dark.rasi
+rofi -no-config -no-lazy-grab -show drun -show-icons -modi drun -theme ~/.config/rofi/config.rasi
 
